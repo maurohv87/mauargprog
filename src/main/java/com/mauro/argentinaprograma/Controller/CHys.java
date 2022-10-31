90c9cfc2b724231ef0a94fcaa9e483657ac623b5
maurohv87 <maurohv_87@hotmail.com>
2022-10-31 18:30:16 -0300

    Comment crosorigin anotation

--- src/main/java/com/mauro/argentinaprograma/Controller/CHys.java
+++ src/main/java/com/mauro/argentinaprograma/Controller/CHys.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://mi-primer-portfolio-543b3.web.app")
+//@CrossOrigin(origins = "https://mi-primer-portfolio-543b3.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
